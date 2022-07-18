9b0bdd5f891efb2e9ff129f6fcd5fcd7932b21c3
OM <olli.multanen@gmail.com>
2022-07-18 19:24:18 +0200

    unicafe step5 done

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,11 +6,16 @@ const Button = (props) => (
   </button>
 )
 
+const StatisticLine = (props) => (
+  <>
+  <p>{props.text} {props.value}</p>
+  </>
+)
+
 const Statistics = (props) => {
   if (props.all.length == 0) {
     return (
       <>
-      <h1>statistics</h1>
       <p>No feedback given</p>
       </>
     )
@@ -18,16 +23,16 @@ const Statistics = (props) => {
 
   return (
     <>
-    <h1>statistics</h1>
-    <p>good {props.good} </p>
-    <p>neutral {props.neutral} </p>
-    <p>bad {props.bad} </p>
-    <p>all {props.all.length}</p>
-    <p>average {props.avgScore}</p>
-    <p>positive {props.avgPositive} %</p>
+    <StatisticLine text="good" value ={props.good} />
+    <StatisticLine text="neutral" value ={props.neutral} />
+    <StatisticLine text="bad" value ={props.bad} />
+    <StatisticLine text="all" value ={props.all.length} />
+    <StatisticLine text="average" value ={props.avgScore} />
+    <StatisticLine text="positive" value ={props.avgPositive}/>
     </>
   )
 }
+
 
 const App = () => {
 
@@ -36,7 +41,7 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState([])
 
-  const avgPositive = good / all.length * 100
+  const avgPositive = good / all.length * 100 + " %"
   const num1 = good - bad
   const avgScore = num1 / all.length
 
@@ -60,6 +65,7 @@ const App = () => {
     <Button handleClick={handleClickGood} text="good"/>
     <Button handleClick={handleClickNeutral} text="neutral"/>
     <Button handleClick={handleClickBad} text="bad"/>
+    <h1>statistics</h1>
     <Statistics good={good}
                 neutral={neutral}
                 bad={bad} 
